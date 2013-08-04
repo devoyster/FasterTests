4b70558db37a7e576fe76d1538167fdb61e8d1c7
Andriy Kozachuk <dev.oyster@gmail.com>
2013-08-04 03:10:21 +0300

    Added previously ignored TestResult.cs

---
+++ Source/Core/Models/TestResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Funt.Core.Models
+{
+    [Serializable]
+    public class TestResult
+    {
+        public TestDescriptor Test { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsIgnored { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
