28721027407b0f9dec364a8f76a789a49157e67a
Andriy Kozachuk <dev.oyster@gmail.com>
2013-08-07 00:01:04 +0300

    Swiched to LightInject DI container

--- Source/Core/Implementation/Infrastructure/DependenciesConfigurator.cs
+++ Source/Core/Implementation/Infrastructure/DependenciesConfigurator.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using FasterTests.Core.Implementation.Integration.Nunit;
-using FasterTests.Core.Implementation.Workers;
-using FasterTests.Core.Interfaces;
-using FasterTests.Core.Interfaces.Integration;
 using FasterTests.Core.Interfaces.Settings;
-using FasterTests.Core.Interfaces.Workers;
-using SimpleInjector;
 
 namespace FasterTests.Core.Implementation.Infrastructure
 {
-    public class DependenciesConfigurator
+    internal class DependenciesConfigurator
     {
         private readonly TestRunSettings _settings;
 
@@ -18,26 +12,17 @@ namespace FasterTests.Core.Implementation.Infrastructure
             _settings = settings;
         }
 
-        public Container ConfigureContainer()
+        public IServiceContainer ConfigureContainer()
         {
-            var container = new Container();
+            var container = new ServiceContainer();
 
-            container.Register<ITestRunnerEntryPoint>(() => new TestRunnerEntryPoint(_settings.AssemblyPath,
-                                                                                     container.GetInstance<ITestInspector>(),
-                                                                                     container.GetInstance<ITestDispatcher>(),
-                                                                                     container.GetInstance<ITestResultsConsumer>()));
+            container.Register(_settings.AssemblyPath, "testAssemblyPath");
+            container.Register(_settings.NoParallelGroups, "noParallelGroups");
+            container.Register(_settings.ConfigStringsToPatch, "configStringsToPatch");
 
-            container.Register<ITestDispatcher>(() => new TestDispatcher(container.GetInstance<ITestWorkersPool>(),
-                                                                         _settings.NoParallelGroups));
+            container.Register(Console.Out, "output");
 
-            container.Register<ITestResultsConsumer>(() => new TestResultsConsoleWriter(Console.Out));
-
-            container.Register<ITestWorkersPool>(() => new TestWorkersPool(_settings.ConfigStringsToPatch));
-
-            container.Register<ITestInspector, NunitTestInspector>();
-            container.Register<ITestEngine, NunitTestEngine>();
-
-            container.Verify();
+            container.RegisterAssembly(GetType().Assembly);
 
             return container;
         }
