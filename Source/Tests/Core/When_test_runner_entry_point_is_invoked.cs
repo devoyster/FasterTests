4a36fb8bec05a15dfa9e9c9be2a7624749c057e0
Andriy Kozachuk <dev.oyster@gmail.com>
2013-08-08 00:01:19 +0300

    Added tests for test runner entry point (MSpec.Fakes)

---
+++ Source/Tests/Core/When_test_runner_entry_point_is_invoked.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FasterTests.Core.Implementation;
+using FasterTests.Core.Interfaces;
+using FasterTests.Core.Interfaces.Integration;
+using FasterTests.Core.Interfaces.Models;
+using Machine.Fakes;
+using Machine.Specifications;
+
+namespace FasterTests.Tests.Core
+{
+    [Subject(typeof(TestRunnerEntryPoint))]
+    public class When_test_runner_entry_point_is_invoked : WithSubject<TestRunnerEntryPoint>
+    {
+        Because of = () =>
+            Subject.Run();
+
+        It should_inspect_tests_once = () => The<ITestInspector>().WasToldTo(i => i.LoadAllTestsFrom(Param.IsAny<string>())).OnlyOnce();
+
+        It should_run_tests_once = () => The<ITestDispatcher>().WasToldTo(i => i.RunTests(Param.IsAny<IEnumerable<TestDescriptor>>())).OnlyOnce();
+
+        It should_consume_tests_once = () => The<ITestResultsConsumer>().WasToldTo(i => i.Consume(Param.IsAny<IEnumerable<TestResult>>())).OnlyOnce();
+    }
+}
