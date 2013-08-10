7eb106b9d8002400e47664332d46b3d45cd7c9f0
Andriy Kozachuk <dev.oyster@gmail.com>
2013-08-10 23:51:45 +0300

    Added IoC container tests

---
+++ Source/Tests/Core/ServiceContainerSpecs/TestServices/ServiceWithNamedParameters.cs
@@ -0,0 +1,24 @@
+﻿namespace FasterTests.Tests.Core.ServiceContainerSpecs.TestServices
+{
+    public class ServiceWithNamedParameters
+    {
+        private readonly string _first;
+        private readonly string _second;
+
+        public ServiceWithNamedParameters(string first, string second)
+        {
+            _first = first;
+            _second = second;
+        }
+
+        public string First
+        {
+            get { return _first; }
+        }
+
+        public string Second
+        {
+            get { return _second; }
+        }
+    }
+}
---
+++ Source/Tests/Core/ServiceContainerSpecs/When_named_services_are_registered.cs
@@ -0,0 +1,30 @@
+﻿using FasterTests.Core;
+using FasterTests.Tests.Core.ServiceContainerSpecs.TestServices;
+using Machine.Specifications;
+
+namespace FasterTests.Tests.Core.ServiceContainerSpecs
+{
+    [Subject(typeof(ServiceContainer))]
+    public class When_named_services_are_registered
+    {
+        Establish context = () =>
+        {
+            container = new ServiceContainer();
+
+            container.Register("string #1", "first");
+            container.Register("string #2", "second");
+
+            container.Register<ServiceWithNamedParameters>();
+        };
+
+        Because of = () =>
+            service = container.GetInstance<ServiceWithNamedParameters>();
+
+        It should_inject_first_dependency_by_name = () => service.First.ShouldEqual("string #1");
+
+        It should_inject_second_dependency_by_name = () => service.Second.ShouldEqual("string #2");
+
+        private static ServiceContainer container;
+        private static ServiceWithNamedParameters service;
+    }
+}
