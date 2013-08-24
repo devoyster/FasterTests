9a419771af5065aa1b5f3459457461cb073a1561
Andriy Kozachuk <dev.oyster@gmail.com>
2013-08-24 22:01:35 +0300

    Removed unused code

--- Source/Helpers/EnumerableExtensions.cs
+++ Source/Helpers/EnumerableExtensions.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace FasterTests.Helpers
 {
@@ -12,24 +11,6 @@ namespace FasterTests.Helpers
             {
                 action(item);
             }
-        }
-
-        public static IEnumerable<IEnumerable<T>> SplitInEqualBatches<T>(this IEnumerable<T> source, int batchCount)
-        {
-            var sourceArray = source.ToArray();
-
-            var batchSize = sourceArray.Length / batchCount + 1;
-            var batches = Enumerable
-                            .Repeat(1, batchCount)
-                            .Select(_ => new List<T>(batchSize))
-                            .ToArray();
-
-            for (int i = 0; i < sourceArray.Length; i++)
-            {
-                batches[i / batchSize].Add(sourceArray[i]);
-            }
-
-            return batches;
         }
     }
 }
